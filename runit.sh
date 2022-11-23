12ec208c6cd5c8c4efea8272096053bbc1eee81e
Chris Clark <chris.clark@ostmodern.co.uk>
2022-11-23 12:11:59 +0000

    removed next image

---
+++ runit.sh
@@ -0,0 +1,7 @@
+echo "# amplifynextjs" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:globalstripe/amplifynextjs.git
+git push -u origin main
