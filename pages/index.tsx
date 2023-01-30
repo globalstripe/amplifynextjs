c9a986332bab47304f90995af87c1b8530c7dc2d
Chris Clark <chris.clark@ostmodern.co.uk>
2023-01-30 15:28:22 +0000

    Footer Text,removed Examples and deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a> </h1>
           <h1 className={styles.title}>
-          Latest Commit by Chris Clark - Mon 30th Jan
+          Commit Mon 30th Jan
           </h1>
           <h1 className={styles.title}>
-          With a notification maybe?
+          With a notifications
           </h1>
        
         <p className={styles.description}>
@@ -37,41 +37,13 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            
-          </span>
-        </a>
+
+        <p>The Footer</p>  
+      
       </footer>
     </div>
   )
