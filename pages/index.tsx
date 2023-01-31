a7792263abe17ef8ec02009420f4feff9ce31855
Chris Clark <chris.clark@ostmodern.co.uk>
2023-01-31 14:19:53 +0000

    31st Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a> </h1>
           <h1 className={styles.title}>
-          Commit Mon 30th Jan
+          Commit Tues 31st Jan
           </h1>
           <h1 className={styles.title}>
           With a notifications
